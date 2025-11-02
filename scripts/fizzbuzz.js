7c240b928d14834a258eb3f5352fd3ac1ea4d4ee
Kavoya <78526936+Kavoyaa@users.noreply.github.com>
2025-11-02 14:43:38 +0530

    eee

--- scripts/fizzbuzz.js
+++ scripts/fizzbuzz.js
@@ -27,14 +27,14 @@ Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13"
 
 
 const submit_btn = document.getElementById("submit-btn");
-
+const result_popup = document.getElementById("result-popup");
 
 
 
 async function submit_btn_click() {
     var code = document.getElementById("code-editor").value;
 
-    const request = new Request("https://slopcode-api.vercel.app/gen", {
+    const request = new Request("http://127.0.0.1:5000/gen", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({
@@ -46,6 +46,14 @@ async function submit_btn_click() {
     const result = await fetch(request);
     const res = await result.json();
     console.log(res)
+
+    result_popup.style.display = "flex";
+    result_popup.innerHTML = `
+    <h3>Your code is</h3>
+    <h1>${res.rating}% SLOP</h1>
+    `;
 }
 
 submit_btn.addEventListener("click", submit_btn_click);
+
+
---
+++ scripts/reverse.js
@@ -0,0 +1,39 @@
+const problem = `
+Reverse the given string.
+
+Example:
+Input: hello
+Output: olleh
+`
+
+const submit_btn = document.getElementById("submit-btn");
+const result_popup = document.getElementById("result-popup");
+
+
+
+async function submit_btn_click() {
+    var code = document.getElementById("code-editor").value;
+
+    const request = new Request("http://127.0.0.1:5000/gen", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            problem: problem,
+            code: code
+        })
+    });
+
+    const result = await fetch(request);
+    const res = await result.json();
+    console.log(res)
+
+    result_popup.style.display = "flex";
+    result_popup.innerHTML = `
+    <h3>Your code is</h3>
+    <h1>${res.rating}% SLOP</h1>
+    `;
+}
+
+submit_btn.addEventListener("click", submit_btn_click);
+
+
